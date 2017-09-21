9349308894958adc480d3d107ffb12b947111f51
Paul McLear <paul.mclear@gmail.com>
2017-09-21 18:30:15 +0000

    Section 13 + remove env.

--- application.py
+++ application.py
@@ -3,9 +3,12 @@ from flask.ext.mongoengine import MongoEngine
 
 db = MongoEngine()
 
-def create_app():
+def create_app(**config_overrides):
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
+    
+    # Update config where test parameters are fed into
+    app.config.update(config_overrides)
     
     # Db setup
     db.init_app(app)

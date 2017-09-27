765571fa2eae78bf43bedbed572487ce1c785a85
Paul McLear <paul.mclear@gmail.com>
2017-09-27 21:23:12 +0000

    Lesson 19 completed.

--- user/forms.py
+++ user/forms.py
@@ -1,11 +1,13 @@
 from flask_wtf import Form
 from wtforms import validators, StringField, PasswordField
+from wtforms.widgets import TextArea
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import ValidationError
+import re
 
 from user.models import User
 
-class RegisterForm(Form):
+class BaseUserForm(Form):
     first_name = StringField('First Name', [validators.DataRequired()])
     last_name = StringField('Last Name', [validators.DataRequired()])
     email = EmailField('Email Address', [
@@ -18,6 +20,12 @@ class RegisterForm(Form):
         validators.length(min=4, max=25)
         ]
     )
+    bio = StringField('Bio',
+        widget=TextArea(),
+        validators=[validators.Length(max=160)]
+    )
+    
+class RegisterForm(BaseUserForm):
     password = PasswordField('New Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm',message='Passwords must match'),
@@ -29,6 +37,8 @@ class RegisterForm(Form):
     def validate_username(form, field):
         if User.objects.filter(username=field.data).first():
             raise ValidationError("Username is already taken")
+        if not re.match("^[a-zA-Z0-9_-]{4,25}$",field.data):
+            raise ValidationError("Invalid username")
     
     def validate_email(form, field):
         if User.objects.filter(email=field.data).first():
@@ -45,3 +55,6 @@ class LoginForm(Form):
         validators.length(min=4, max=80)
         ]
     )
+    
+class EditForm(BaseUserForm):
+    pass
--- user/views.py
+++ user/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, session
+from flask import Blueprint, render_template, request, redirect, session, url_for, abort
 import bcrypt
 
-from user.forms import RegisterForm, LoginForm
 from user.models import User
+from user.forms import RegisterForm, LoginForm, EditForm
 
 user_app = Blueprint('user_app', __name__)
 
@@ -45,3 +45,46 @@ def register():
         return "User registered!"
     return render_template('user/register.html', form=form)
     
+@user_app.route('/logout',methods=('GET','POST'))
+def logout():
+    session.pop('username')
+    return redirect(url_for('user_app.login'))
+    
+@user_app.route('/<username>', methods=('GET','POST'))
+def profile(username):
+    edit_profile = False
+    user = User.objects.filter(username=username).first()
+    if user.username == session.get('username') and session.get('username'):
+        edit_profile = True
+    if user:
+        return render_template('user/profile.html', user=user, edit_profile=edit_profile)
+    else:
+        abort(404)
+        
+@user_app.route('/edit',methods=('GET','POST'))
+def edit():
+    error = None
+    message = None
+    user = User.objects.filter(username=session.get('username')).first()
+    if user:
+        form = EditForm(obj=user)
+        if form.validate_on_submit():
+            if user.username != form.username.data:
+                if User.objects.filter(username=form.username.data.lower()).first():
+                    error = "Username already exists"
+                else:
+                    session['username'] = form.username.data.lower()
+                    form.username.data = form.username.data.lower()
+            if user.email != form.email.data:
+                if User.objects.filter(email=form.email.data.lower()).first():
+                    error = "Email already exists"
+                else:
+                    form.email.data = form.email.data.lower()
+            if not error:
+                form.populate_obj(user)
+                user.save()
+                message = "Profile updated"
+        return render_template('user/edit.html',form=form, error=error, message=message)
+    else:
+        abort(404)
+        

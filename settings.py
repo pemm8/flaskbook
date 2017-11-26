99442829c8a2d0787bd9237ad39df4f1120135df
Paul McLear <paul.mclear@gmail.com>
2017-11-26 21:32:01 +0000

    Complete up to unit 43.

--- settings.py
+++ settings.py
@@ -4,3 +4,12 @@ MONGODB_DB = 'flaskbook'
 HOSTNAME = 'https://flaskbook-pemm8.c9users.io'
 UPLOAD_FOLDER = '/home/ubuntu/workspace/flaskbook/static/images'
 STATIC_IMAGE_URL = 'images'
+
+# AWS_BUCKET = 'flaskbook-app1987'
+# AWS_CONTENT_URL = 'https://s3-eu-west-1.amazonaws.com/'
+
+AWS_BUCKET = ''
+AWS_CONTENT_URL = ''
+
+# https://s3-eu-west-1.amazonaws.com/flaskbook-app1987/user/5a114947f4b049105f41973a.1511698432.raw.png
+# https://s3-eu-west-1.amazonaws.com/flaskbook-app1987/user/5a114947f4b049105f41973a.1511698432.xlg.png
--- user/views.py
+++ user/views.py
@@ -9,6 +9,8 @@ from user.forms import RegisterForm, LoginForm, EditForm, ForgotForm, PasswordRe
 from utilities.common import email
 from settings import UPLOAD_FOLDER
 from utilities.imaging import thumbnail_process
+from relationship.models import Relationship
+from user.decorators import login_required
 
 user_app = Blueprint('user_app', __name__)
 
@@ -70,15 +72,20 @@ def logout():
 @user_app.route('/<username>', methods=('GET','POST'))
 def profile(username):
     edit_profile = False
+    rel = None
     user = User.objects.filter(username=username).first()
+    if user.username == session.get('username') and session.get('username'):
+        edit_profile = True
     if user:
-        if user.username == session.get('username') and session.get('username'):
-            edit_profile = True
-        return render_template('user/profile.html', user=user, edit_profile=edit_profile)
+        if session.get('username'):
+            logged_user = User.objects.filter(username=session.get('username')).first()
+            rel = Relationship.get_relationship(logged_user, user)
+        return render_template('user/profile.html', user=user, rel=rel, edit_profile=edit_profile)
     else:
         abort(404)
-        
+
 @user_app.route('/edit',methods=('GET','POST'))
+@login_required 
 def edit():
     error = None
     message = None
